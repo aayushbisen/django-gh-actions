ada0c03fc315a07a804e2c3d2b302276393ca201
Aayush Bisen <aayush23101998@outlook.com>
2021-01-27 00:40:03 +0530

    Added unit test and increased test coverage

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+
+  def setUp(self):
+    self.post = Post.objects.create(title='Django',author='Aayush',slug='django')
+
+
+  def test_post_model(self):
+    d = self.post
+    self.assertTrue(isinstance(d, Post))
+    self.assertEqual(str(d), 'Django')
