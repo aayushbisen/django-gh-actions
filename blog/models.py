1b869a4ebcf593d4b9eff4db633fa74f5f9d0272
Aayush Bisen <aayush23101998@outlook.com>
2021-01-27 00:57:44 +0530

    Added linting using flake8

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Post(models.Model):
-  title = models.CharField(max_length=255)
-  author = models.CharField(max_length=255)
-  slug = models.SlugField(max_length=255)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -5,11 +5,10 @@ from .models import Post
 
 class PostModelTest(TestCase):
 
-  def setUp(self):
-    self.post = Post.objects.create(title='Django',author='Aayush',slug='django')
+    def setUp(self):
+        self.post = Post.objects.create(title='Django', author='Aayush', slug='django')
 
-
-  def test_post_model(self):
-    d = self.post
-    self.assertTrue(isinstance(d, Post))
-    self.assertEqual(str(d), 'Django')
+    def test_post_model(self):
+        d = self.post
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'Django')
